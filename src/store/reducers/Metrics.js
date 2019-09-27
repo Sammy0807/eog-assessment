9ef147efdd643adac5218440f1aa1da557318d07
Sammy0807 <sami.egwu90@gmail.com>
2019-09-27 11:05:10 -0500

    this commits

--- src/store/reducers/Metrics.js
+++ src/store/reducers/Metrics.js
@@ -29,3 +29,84 @@ const initialState = {
   time: null,
   error: false
 };
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_TURBING_PRESSURE:
+      return {
+        //
+      };
+    case ADD_TURBING_PRESSURE:
+      return {
+        //
+      };
+    case DELETE_TURBING_PRESSURE:
+      return {
+        //
+      };
+    case GET_FLARETEMP:
+      return {
+        //
+      };
+    case ADD_FLARETEMP:
+      return {
+        //
+      };
+    case DELETE_FLARETEMP:
+      return {
+        //
+      };
+    case GET_INJVALVEOPEN:
+      return {
+        //
+      };
+    case ADD_INJVALVEOPEN:
+      return {
+        //
+      };
+    case DELETE_INJVALVEOPEN:
+      return {
+        //
+      };
+    case GET_OILTEMP:
+      return {
+        //
+      };
+    case ADD_OILTEMP:
+      return {
+        //
+      };
+    case DELETE_OILTEMP:
+      return {
+        //
+      };
+    case GET_CASINGPRESSURE:
+      return {
+        //
+      };
+    case ADD_CASINGPRESSURE:
+      return {
+        //
+      };
+    case DELETE_CASINGPRESSURE:
+      return {
+        //
+      };
+    case GET_WATERTEMP:
+      return {
+        //
+      };
+    case ADD_WATERTEMP:
+      return {
+        //
+      };
+    case DELETE_WATERTEMP:
+      return {
+        //
+      };
+    case DELETE_OILTEMP:
+      return {
+        //
+      };
+  }
+};
